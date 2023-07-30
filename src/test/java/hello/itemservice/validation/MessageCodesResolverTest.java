c39fde20f0565f92ed6e33e123fe400f06acfe68
SangCheol <scpark0698@gmail.com>
2023-07-31 02:15:10 +0900

    오류 메시지 처리 ~v6, validator 추가

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -19,6 +19,9 @@ public class MessageCodesResolverTest {
     @Test
     void messageCodesResolverField() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        for (String messageCode : messageCodes) {
+            System.out.println("messageCode = " + messageCode);
+        }
         assertThat(messageCodes).containsExactly(
                 "required.item.itemName",
                 "required.itemName",
